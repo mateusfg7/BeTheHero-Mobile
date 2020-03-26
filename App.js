99866d25c30417c3641e515af03621e897f6fb82
mateusfg7 <mateusfelipefg77@gmail.com>
2020-03-26 10:50:11 -0300

    initial files for pages

--- App.js
+++ App.js
@@ -1,25 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello Word!</Text>
+    <View>
+      <Text >Hello Word!</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  text: {
-    color: '#fff',
-    fontSize: 20,
-    fontWeight: 'bold'
-  }
-});
